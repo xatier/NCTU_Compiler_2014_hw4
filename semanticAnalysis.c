00eec90ad17292a87b4046ec2003e50970c1c172
xatier <xatierlike@gmail.com>
2014-05-15 23:57:33 +0800

    stmt & block
    style fix

--- semanticAnalysis.c
+++ semanticAnalysis.c
@@ -63,8 +63,7 @@ typedef enum ErrorMsgKind
     PASS_SCALAR_TO_ARRAY
 } ErrorMsgKind;
 
-void printErrorMsgSpecial(AST_NODE* node1, char* name2, ErrorMsgKind errorMsgKind)
-{
+void printErrorMsgSpecial (AST_NODE *node1, char *name2, ErrorMsgKind errorMsgKind) {
     g_anyErrorOccur = 1;
     printf("Error found in line %d\n", node1->linenumber);
     /*
@@ -78,8 +77,7 @@ void printErrorMsgSpecial(AST_NODE* node1, char* name2, ErrorMsgKind errorMsgKin
 }
 
 
-void printErrorMsg(AST_NODE* node, ErrorMsgKind errorMsgKind)
-{
+void printErrorMsg (AST_NODE *node, ErrorMsgKind errorMsgKind) {
     g_anyErrorOccur = 1;
     printf("Error found in line %d\n", node->linenumber);
     /*
@@ -92,14 +90,12 @@ void printErrorMsg(AST_NODE* node, ErrorMsgKind errorMsgKind)
 }
 
 
-void semanticAnalysis(AST_NODE *root)
-{
+void semanticAnalysis (AST_NODE *root) {
     processProgramNode(root);
 }
 
 
-DATA_TYPE getBiggerType(DATA_TYPE dataType1, DATA_TYPE dataType2)
-{
+DATA_TYPE getBiggerType (DATA_TYPE dataType1, DATA_TYPE dataType2) {
     if(dataType1 == FLOAT_TYPE || dataType2 == FLOAT_TYPE) {
         return FLOAT_TYPE;
     } else {
@@ -108,10 +104,9 @@ DATA_TYPE getBiggerType(DATA_TYPE dataType1, DATA_TYPE dataType2)
 }
 
 
-void processProgramNode(AST_NODE *programNode)
-{
-    AST_NODE* child = programNode->child;
-    while(child) {
+void processProgramNode (AST_NODE *programNode) {
+    AST_NODE *child = programNode->child;
+    while (child) {
         processDeclarationNode(programNode->child);
         child = child->rightSibling;
     }
@@ -119,115 +114,157 @@ void processProgramNode(AST_NODE *programNode)
 
 void processDeclarationNode(AST_NODE* declarationNode)
 {
-    if(declarationNode->nodeType == DECLARATION_NODE) {
-        AST_NODE* child = declarationNode->child;
+    if (declarationNode->nodeType == DECLARATION_NODE) {
+        AST_NODE *child = declarationNode->child;
         processTypeNode(child);
-        
-    }         
+    }
 }
 
 
-void processTypeNode(AST_NODE* idNodeAsType)
-{
+void processTypeNode (AST_NODE *idNodeAsType) {
 }
 
 
-void declareIdList(AST_NODE* declarationNode, SymbolAttributeKind isVariableOrTypeAttribute, int ignoreArrayFirstDimSize)
-{
+void declareIdList (AST_NODE *declarationNode, SymbolAttributeKind isVariableOrTypeAttribute, int ignoreArrayFirstDimSize) {
 }
 
-void checkAssignOrExpr(AST_NODE* assignOrExprRelatedNode)
-{
+void checkAssignOrExpr (AST_NODE *assignOrExprRelatedNode) {
 }
 
-void checkWhileStmt(AST_NODE* whileNode)
-{
+void checkWhileStmt (AST_NODE *whileNode) {
 }
 
 
-void checkForStmt(AST_NODE* forNode)
-{
+void checkForStmt (AST_NODE *forNode) {
 }
 
 
-void checkAssignmentStmt(AST_NODE* assignmentNode)
-{
+void checkAssignmentStmt (AST_NODE *assignmentNode) {
 }
 
 
-void checkIfStmt(AST_NODE* ifNode)
-{
+void checkIfStmt(AST_NODE *ifNode) {
 }
 
-void checkWriteFunction(AST_NODE* functionCallNode)
-{
+void checkWriteFunction(AST_NODE *functionCallNode) {
 }
 
-void checkFunctionCall(AST_NODE* functionCallNode)
-{
+void checkFunctionCall (AST_NODE *functionCallNode) {
 }
 
-void checkParameterPassing(Parameter* formalParameter, AST_NODE* actualParameter)
-{
+void checkParameterPassing (Parameter *formalParameter, AST_NODE *actualParameter) {
 }
 
 
-void processExprRelatedNode(AST_NODE* exprRelatedNode)
-{
+void processExprRelatedNode (AST_NODE *exprRelatedNode) {
 }
 
-void getExprOrConstValue(AST_NODE* exprOrConstNode, int* iValue, float* fValue)
-{
+void getExprOrConstValue (AST_NODE *exprOrConstNode, int *iValue, float *fValue) {
 }
 
-void evaluateExprValue(AST_NODE* exprNode)
-{
+void evaluateExprValue (AST_NODE *exprNode) {
 }
 
 
-void processExprNode(AST_NODE* exprNode)
-{
+void processExprNode (AST_NODE *exprNode) {
 }
 
 
-void processVariableLValue(AST_NODE* idNode)
-{
+void processVariableLValue (AST_NODE *idNode) {
 }
 
-void processVariableRValue(AST_NODE* idNode)
-{
-}
-
-
-void processConstValueNode(AST_NODE* constValueNode)
-{
+void processVariableRValue (AST_NODE *idNode) {
 }
 
 
-void checkReturnStmt(AST_NODE* returnNode)
-{
+void processConstValueNode (AST_NODE *constValueNode) {
 }
 
 
-void processBlockNode(AST_NODE* blockNode)
-{
+void checkReturnStmt (AST_NODE *returnNode) {
 }
 
 
+// xatier
+// block
+//     : decl_list stmt_list
+//     | stmt_list
+//     | decl_list
+void processBlockNode (AST_NODE *blockNode) {
+    AST_NODE *child = blockNode->child;
+    while (child) {
+        switch (child->nodeType) {
+            case VARIABLE_DECL_LIST_NODE:
+                processDeclDimList(child);
+                break;
+            case STMT_LIST_NODE:
+                processStmtNode(child);
+                break;
+            default:
+                break;
+        }
+
+        child = child->rightSibling;
+    }
+}
+
+
+// xatier
+// stmt
+//     : { block }
+//     | while ( relop_expr ) { block }
+//     | for ( assign_expr_list ; relop_expr_list ; assign_expr_list ) { block }
+//     | var_ref = relop_expr ;
+//     | if ( relop_expr ) { block }
+//     | if ( relop_expr ) ;
+//     | if ( relop_expr ) { block } else stmt
+//     | id ( relop_expr_list ) ;                 // function call
+//     | ;
+//     | return ;
+//     | return relop_expr ;
 void processStmtNode(AST_NODE* stmtNode)
 {
+    AST_NODE *child = stmtNode->child;
+    while (child) {
+        if (child->nodeType == BLOCK_NODE) {
+            processBlockNode(child);
+        }
+        else {
+            switch (child->semantic_value.stmtSemanticValue.kind) {
+                case WHILE_STMT:
+                    checkWhileStmt(child);
+                    break;
+                case FOR_STMT:
+                    checkForStmt(child);
+                    break;
+                case ASSIGN_STMT:
+                    checkAssignmentStmt(child);
+                    break;
+                case IF_STMT:
+                    checkIfStmt(child);
+                    break;
+                case FUNCTION_CALL_STMT:
+                    checkFunctionCall(child);
+                    break;
+                case RETURN_STMT:
+                    checkReturnStmt(child);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        child = child->rightSibling;
+    }
 }
 
 
-void processGeneralNode(AST_NODE *node)
-{
+void processGeneralNode (AST_NODE *node) {
 }
 
-void processDeclDimList(AST_NODE* idNode, TypeDescriptor* typeDescriptor, int ignoreFirstDimSize)
-{
+void processDeclDimList (AST_NODE *idNode, TypeDescriptor *typeDescriptor, int ignoreFirstDimSize) {
 }
 
 
-void declareFunction(AST_NODE* declarationNode)
-{
+void declareFunction (AST_NODE *declarationNode) {
 }
